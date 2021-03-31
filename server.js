a2753912c241ba7e102fcdb43bc9bc998766ce93
Glitch (hello-express) <none>
2021-03-31 13:20:21 +0000

    🌁🚤 Checkpoint ./msal-auth.js:226065/2478 ./server.js:226065/762

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const msal = require('@azure/msal-node');
 const cookieSession = require("cookie-session");
 const config = require('./msal-config')(msal)
+const msalAuth = require('./msal-auth');
 app.use(cookieSession({
   name: 'auth',
   keys: [process.env.COOKIE_KEY],
-  maxAge: 168 * 60 * 60 * 1000 // 24*7 hours
+  maxAge: 168 * 60 * 60 * 1000, // 24*7 hours
+  sameSite: true,
+  secure: true
 }))
 
 
@@ -36,25 +39,11 @@ app.get('/', (req, res) => {
     }
 });
 
-app.get('/redirect', (req, res, next) => {
-    const tokenRequest = {
-        code: req.query.code,
-        scopes: ["user.read"],
-        redirectUri: "https://eoys-uploader-2021.glitch.me/redirect",
-    };
+app.get('/redirect', msalAuth.redirect)
 
-    cca.acquireTokenByCode(tokenRequest).then((response) => {
-        console.log("\nResponse: \n:", response);
-        
-        req.session.user = {
-          name: response.account.name,
-          email: response.account.username
-        }
-        return res.redirect('/')
-    }).catch((error) => {
-        console.log(error);
-        return res.status(500).send(error);
-    });
+app.get('/logout', (req, res)=>{
+  req.session = null
+  res.redirect('/')
 })
 
 // listen for requests :)
