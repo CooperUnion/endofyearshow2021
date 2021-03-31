e00baff84c1653ff189f73b61c89059661877a61
Glitch (hello-express) <none>
2021-03-31 02:42:08 +0000

    🔔💰 Checkpoint ./server.js:226065/2550

--- server.js
+++ server.js
@@ -7,11 +7,24 @@ const express = require("express");
 const app = express();
 const msal = require('@azure/msal-node');
 
+/*
+
+.env vars
+APPLICATION_CLIENT_ID
+DIRECTORY_ID
+OBJECT_ID
+TENANT_ID
+CLOUD_INSTANCE_ID
+CLIENT_SECRET_VALUE
+CLIENT_SECRET_ID
+
+*/
+
 const config = {
     auth: {
         clientId: process.env.APPLICATION_CLIENT_ID,
-        authority: `Enter_the_Cloud_Instance_Id_Here/Enter_the_Tenant_Id_here`,
-        clientSecret: "Enter_the_Client_secret"
+        authority: `${process.env.CLOUD_INSTANCE_ID}/${process.env.TENANT_ID}`,
+        clientSecret: process.env.CLIENT_SECRET_VALUE
     },
     system: {
         loggerOptions: {
@@ -30,16 +43,35 @@ const config = {
 // https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+const cca = new msal.ConfidentialClientApplication(config);
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
+  app.get('/', (req, res) => {
+      const authCodeUrlParameters = {
+          scopes: ["user.read"],
+          redirectUri: "https://eoys-uploader-2021.glitch.me/redirect",
+      };
+
+      // get url to sign user in and consent to scopes needed for application
+      cca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
+          res.redirect(response);
+      }).catch((error) => console.log(JSON.stringify(error)));
+  });
+
+  app.get('/redirect', (req, res) => {
+      const tokenRequest = {
+          code: req.query.code,
+          scopes: ["user.read"],
+          redirectUri: "https://eoys-uploader-2021.glitch.me/redirect",
+      };
+
+      cca.acquireTokenByCode(tokenRequest).then((response) => {
+          console.log("\nResponse: \n:", response);
+          res.sendStatus(200);
+      }).catch((error) => {
+          console.log(error);
+          res.status(500).send(error);
+      });
+  });
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
