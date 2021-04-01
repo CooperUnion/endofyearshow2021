0ae3c3dd16bce017748b14500571afd45f632d84
Glitch (hello-express) <none>
2021-04-01 01:14:12 +0000

    🚲📌 Checkpoint ./server.js:226065/350

--- server.js
+++ server.js
@@ -20,11 +20,7 @@ app.get('/redirect', msalAuth.redirect)
 
 app.get('/logout', (req, res)=>{
   req.session = null
-  
-  /*
-     "https://login.microsoftonline.com/common/oauth2/v2.0/logout"
-        "?post_logout_redirect_uri=" + url_for("index", _external=True))
-  */
+
   res.redirect('https://login.microsoftonline.com/common/oauth2/v2.0/logout?post_logout_redirect_uri=https://eoys-uploader-2021.glitch.me/')
 })
 
