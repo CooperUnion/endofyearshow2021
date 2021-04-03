31683c8732bf0de73f7b98e1967571dc6a338b50
Glitch (hello-express) <none>
2021-04-03 21:44:50 +0000

    🍉👘 Checkpoint ./views/form.html:20381007/218014 ./formsfx.js:20381007/1999 ./public/formsfx.js:20381007/1999 ./public/formstyles.css:20381007/608

---
+++ public/formsfx.js
@@ -0,0 +1,58 @@
+/*jshint esversion: 6 */
+
+const FormFX = function() {
+
+  if (!Element.prototype.matches) Element.prototype.matches = Element.prototype.msMatchesSelector;
+  if (!Element.prototype.closest) Element.prototype.closest = function(selector) {
+    var el = this;
+    while (el) {
+      if (el.matches(selector)) {
+        return el;
+      }
+      el = el.parentElement;
+    }
+  };
+
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+    console.info('polyfill for IE11');
+    NodeList.prototype.forEach = function(callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    };
+  }
+
+  const worktypeRadio = document.querySelector("fieldset.section-typeofwork .inputlist.radio");
+  worktypeRadio.addEventListener("click", handleFieldsetVisibility);
+  
+  const specialRadioText = document.querySelector(".special.radio-text input[type='text']");
+  specialRadioText.addEventListener("focus", forceRadioCheck);
+  
+	document.querySelector("fieldset.section-standardwork").classList.add("hide");
+	document.querySelector("fieldset.section-videowork").classList.remove("hide");
+
+  function handleFieldsetVisibility() {
+    const workTypeChecked = worktypeRadio.querySelector("input:checked");
+    const typeOfWork = workTypeChecked === null ? "not selected" : workTypeChecked.value;
+
+    switch (typeOfWork) {
+    case "video":
+      document.querySelector("fieldset.section-standardwork").classList.add("hide");
+      document.querySelector("fieldset.section-videowork").classList.remove("hide");
+      break;
+
+    default:
+      document.querySelector("fieldset.section-standardwork").classList.remove("hide");
+      document.querySelector("fieldset.section-videowork").classList.add("hide");
+    }
+  }
+  
+	function forceRadioCheck(e) {
+		this.closest(".special").querySelector("input[type='radio']").checked = true;
+	}
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+  new FormFX;
+});
