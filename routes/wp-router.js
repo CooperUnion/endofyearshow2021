6270bd7218292d3b7be561cd354d2de87457b0de
Glitch (eoys-uploader-2021) <none>
2021-04-19 03:08:14 +0000

    👖🚄 Checkpoint ./lib/wp.js:226065/90 ./routes/wp-router.js:226065/257

--- routes/wp-router.js
+++ routes/wp-router.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const {getAllTags} = require('../lib/wp');
+const {
+  getAllTags, 
+  getAllCategories, 
+  ...wp
+} = require('../lib/wp');
+
+console.log(wp)
 
 const student_page_limit = 100
-
 
 const wpLogger = (req, res, next) =>{
   console.log('hit /auth', req.path)
@@ -17,7 +22,7 @@ router.get('/', (req, res, next)=>{
 })
 
 router.get('/tags', wpLogger, async (req, res) => {
-  const tags = await getAllTags()  
+  const tags = await getAllCategories()  
   res.json(tags)
 })
 
