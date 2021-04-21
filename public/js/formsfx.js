82a40841c7c554fc96fedfc53a4400410e61426c
Glitch (eoys-uploader-2021) <none>
2021-04-21 21:13:00 +0000

    🍀🛳 Checkpoint ./public/js/formsfx.js:20381007/4404

--- public/js/formsfx.js
+++ public/js/formsfx.js
@@ -149,42 +149,49 @@ const FormFX = function() {
           fileReader.readAsDataURL(file);
         } else {
           fileReader.onload = function() {
-            var blob = new Blob([fileReader.result], {type: file.type});
-            var url = URL.createObjectURL(blob);
-            var video = document.createElement('video');
-            var timeupdate = function() {
-              if (snapImage()) {
-                video.removeEventListener('timeupdate', timeupdate);
-                video.pause();
-              }
-            };
-            video.addEventListener('loadeddata', function() {
-              if (snapImage()) {
-                video.removeEventListener('timeupdate', timeupdate);
-              }
-            });
-            var snapImage = function() {
-              var canvas = document.createElement('canvas');
-              canvas.width = video.videoWidth;
-              canvas.height = video.videoHeight;
-              canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-              var image = canvas.toDataURL();
-              var success = image.length > 100000;
-              if (success) {
-                var img = document.createElement('img');
-                img.src = image;
-                document.getElementsByTagName('div')[0].appendChild(img);
-                URL.revokeObjectURL(url);
-              }
-              return success;
-            };
-            video.addEventListener('timeupdate', timeupdate);
-            video.preload = 'metadata';
-            video.src = url;
-            // Load video in Safari / IE11
-            video.muted = true;
-            video.playsInline = true;
-            video.play();
+return new Promise((resolve, reject) => {
+              // load the file to a video player
+              const videoPlayer = document.createElement('video');
+              videoPlayer.setAttribute('src', URL.createObjectURL(file));
+              videoPlayer.load();
+              videoPlayer.addEventListener('error', (ex) => {
+                  reject("error when loading video file", ex);
+              });
+              // load metadata of the video to get video duration and dimensions
+              videoPlayer.addEventListener('loadedmetadata', () => {
+                  // seek to user defined timestamp (in seconds) if possible
+                  if (videoPlayer.duration < seekTo) {
+                      reject("video is too short.");
+                      return;
+                  }
+                  // delay seeking or else 'seeked' event won't fire on Safari
+                  setTimeout(() => {
+                    videoPlayer.currentTime = seekTo;
+                  }, 200);
+                  // extract video thumbnail once seeking is complete
+                  videoPlayer.addEventListener('seeked', () => {
+                      console.log('video is now paused at %ss.', seekTo);
+                      // define a canvas to have the same dimension as the video
+                      const canvas = document.createElement("canvas");
+                      canvas.width = videoPlayer.videoWidth;
+                      canvas.height = videoPlayer.videoHeight;
+                      // draw the video frame to canvas
+                      const ctx = canvas.getContext("2d").drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+                      var image = canvas.toDataURL();
+
+
+                      // return the canvas image as a blob
+                      // ctx.canvas.toBlob(
+                      //     blob => {
+                      //         resolve(blob);
+                      //     },
+                      //     "image/jpeg",
+                      //     0.75 /* quality */
+                      // );
+                  });
+              });
+          });
+            
           };
           fileReader.readAsArrayBuffer(file);
         }
@@ -220,16 +227,18 @@ const FormFX = function() {
                       canvas.width = videoPlayer.videoWidth;
                       canvas.height = videoPlayer.videoHeight;
                       // draw the video frame to canvas
-                      const ctx = canvas.getContext("2d");
-                      ctx.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+                      const ctx = canvas.getContext("2d").drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+                      var image = canvas.toDataURL();
+
+
                       // return the canvas image as a blob
-                      ctx.canvas.toBlob(
-                          blob => {
-                              resolve(blob);
-                          },
-                          "image/jpeg",
-                          0.75 /* quality */
-                      );
+                      // ctx.canvas.toBlob(
+                      //     blob => {
+                      //         resolve(blob);
+                      //     },
+                      //     "image/jpeg",
+                      //     0.75 /* quality */
+                      // );
                   });
               });
           });
