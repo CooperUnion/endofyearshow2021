23dea02160deb29ca94b3f4efcaf71227fb7ed5e
Glitch (eoys-uploader-2021) <none>
2021-04-21 21:01:55 +0000

    😚⏲ Checkpoint ./public/js/formsfx.js:20381007/4808

--- public/js/formsfx.js
+++ public/js/formsfx.js
@@ -137,7 +137,61 @@ const FormFX = function() {
 				}
 			}
 
-      function getVideoCover(file, seekTo = 10.0) {
+      function getThumbSrc(file, seekTo = 10.0) {
+        
+        var fileReader = new FileReader();
+        if (file.type.match('image')) {
+          fileReader.onload = function() {
+            var img = document.createElement('img');
+            img.src = fileReader.result;
+// RETURN fileReader.result
+          };
+          fileReader.readAsDataURL(file);
+        } else {
+          fileReader.onload = function() {
+            var blob = new Blob([fileReader.result], {type: file.type});
+            var url = URL.createObjectURL(blob);
+            var video = document.createElement('video');
+            var timeupdate = function() {
+              if (snapImage()) {
+                video.removeEventListener('timeupdate', timeupdate);
+                video.pause();
+              }
+            };
+            video.addEventListener('loadeddata', function() {
+              if (snapImage()) {
+                video.removeEventListener('timeupdate', timeupdate);
+              }
+            });
+            var snapImage = function() {
+              var canvas = document.createElement('canvas');
+              canvas.width = video.videoWidth;
+              canvas.height = video.videoHeight;
+              canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+              var image = canvas.toDataURL();
+              var success = image.length > 100000;
+              if (success) {
+                var img = document.createElement('img');
+                img.src = image;
+                document.getElementsByTagName('div')[0].appendChild(img);
+                URL.revokeObjectURL(url);
+              }
+              return success;
+            };
+            video.addEventListener('timeupdate', timeupdate);
+            video.preload = 'metadata';
+            video.src = url;
+            // Load video in Safari / IE11
+            video.muted = true;
+            video.playsInline = true;
+            video.play();
+          };
+          fileReader.readAsArrayBuffer(file);
+        }
+        
+        
+        
+        
           console.log("getting video cover for file: ", file);
           return new Promise((resolve, reject) => {
               // load the file to a video player
