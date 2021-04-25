738396f7b10de55e287457d25d3141627d03663c
Glitch (eoys-uploader-2021) <none>
2021-04-25 00:13:36 +0000

    🍖🍯 Checkpoint ./public/app/script.js:226065/4532 ./public/app/templates/Desktop.vue:226065/2548 ./views/app.html:226065/1

--- public/app/script.js
+++ public/app/script.js
@@ -1,5 +1,5 @@
 /* global Vue, VueRouter, httpVueLoader, vuescroll, Vuex */
-const Home = httpVueLoader('/app/templates/Home.vue')
+// const Home = httpVueLoader('/templates/Home.vue')
 // const Category = httpVueLoader('/templates/Category.vue')
 // const Tags = httpVueLoader('/templates/Tags.vue')
 // const Areas = httpVueLoader('/templates/Areas.vue')
@@ -7,57 +7,74 @@ const Home = httpVueLoader('/app/templates/Home.vue')
 // const Student = httpVueLoader('/templates/Student.vue')
 // const Work = httpVueLoader('/templates/Work.vue')
 
+console.log("script.js loaded")
+
 // desktop
-// const Desktop = httpVueLoader('/templates/Desktop.vue')
+const Desktop = httpVueLoader('/templates/Desktop.vue')
+// const DesktopStudent = httpVueLoader('/templates/DesktopStudent.vue')
+// const DesktopStudents = httpVueLoader('/templates/DesktopStudents.vue')
+// const DesktopTags = httpVueLoader('/templates/DesktopTags.vue')
+// const DesktopWork = httpVueLoader('/templates/DesktopWork.vue')
+// const DesktopCategory = httpVueLoader('/templates/DesktopCategory.vue')
 
 
 // const Test = httpVueLoader('/templates/Test.vue')
 
 Vue.config.devtools = true
 
+
+
 Vue.use(VueRouter)
 Vue.use(Vuex)
 
 //Vuex configuration
-const store = new Vuex.Store({
-  state: {
-    displayUI: true
-  },
-  mutations: {
-    toggleUI(state){
-      state.displayUI = state.displayUI === false
-    },
-    showUI(state){
-      state.displayUI = true
-    },
-    hideUI(state){
-      state.displayUI = false
-    }
-  }
-})
+// const store = new Vuex.Store({
+//   state: {
+//     displayUI: true,
+//     displayNav: false
+//   },
+//   mutations: {
+//     toggleUI(state){
+//       state.displayUI = state.displayUI === false
+//     },
+//     showUI(state){
+//       state.displayUI = true
+//     },
+//     hideUI(state){
+//       state.displayUI = false
+//     },
+//     showNav(state){
+//       state.displayNav = true
+//     },
+//     hideNav(state){
+//       state.displayNav = false
+//     }
 
-Vue.directive('scroll', {
-  inserted: function (el, binding) {
-    let f = function (evt) {
-      if (binding.value(evt, el)) {
-        window.removeEventListener('scroll', f)
-      }
-    }
-    window.addEventListener('scroll', f)
-  }
-})
+//   }
+// })
+
+// Vue.directive('scroll', {
+//   inserted: function (el, binding) {
+//     let f = function (evt) {
+//       if (binding.value(evt, el)) {
+//         window.removeEventListener('scroll', f)
+//       }
+//     }
+//     window.addEventListener('scroll', f)
+//   }
+// })
 
 
 const routes = [
-  // {path: '/category/students', component: Students},
+  // {path: '/category/students', component: DesktopStudents},
   // {path: '/category/areas', component: Areas},
-  // {path: '/category/:category', component: Category},
+  // {path: '/category/:category', component: DesktopCategory},
   // {path: '/tags/areas', component: Areas},
-  // {path: '/tags/:tag', component: Tags},
-  // {path: '/student/:student', component: Student},  
-  // {path: '/work/:work', component: Work},    
+  // {path: '/tags/:tag', component: DesktopTags},
+  // {path: '/student/:student', component: DesktopStudent},  
+  // {path: '/work/:work', component: DesktopWork},    
   // {path: '/test', component: Test},
-  {path: '/', component: Home},
+  {path: '/', component: Desktop},
   // {path: '/desktop', component: Desktop}
 ]
 
@@ -70,9 +87,9 @@ let $route = router.match(location)
 
 // console.log("matched path", $route.path)
 
-console.log(router)
+
 const app = new Vue({
-  store,
+  // store,
   router,
   template:`<router-view />`
 }).$mount('#app')
