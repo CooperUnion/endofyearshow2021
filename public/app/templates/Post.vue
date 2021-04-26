1021ff2a487c346b0228f67873c70b8ff54b351c
Glitch (eoys-uploader-2021) <none>
2021-04-26 13:47:06 +0000

    👨‍👨‍👦☂️ Checkpoint ./public/app/templates/Post.vue:226065/684

--- public/app/templates/Post.vue
+++ public/app/templates/Post.vue
@@ -11,18 +11,7 @@
     <ul>
       <li class="post" v-for="post in posts">
         
-        <a :href="post.assets.preview.source_url"><img :src="post.assets.preview.thumbnail.source_url" /></a>
-        <pre>
-          id: {{post.id}}
-          title: {{post.title}}
-          type: {{post.type}}
-          author: {{post.author.formatted}}
-          tags: {{post.taxonomy.tags}}
-          description: {{post.meta.description}}
-          url: <a :href="post.assets.url">{{post.assets.url}}</a>
-          high-res: <a :href="post.assets.preview.source_url">{{post.assets.preview.source_url}}</a>
-          <span v-if="post.type === 'video'">video: <a :href="post.assets.media.url">{{post.assets.media.url}}</a></span>
-        </pre>
+        <post></post>
     
       </li>  
     </ul>
@@ -37,7 +26,9 @@
 
 
   module.exports = {
-    components: {},
+    components: {
+      Post
+    },
     data() {
       return {
         loading: true,
