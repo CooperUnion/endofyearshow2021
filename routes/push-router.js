970c695ebf87d9f7dcb18b66ee2ddca518300dd5
Glitch (eoys-uploader-2021) <none>
2021-04-27 04:45:28 +0000

    🚈👜 Checkpoint ./package.json:226065/1566 ./routes/push-router.js:226065/970

--- routes/push-router.js
+++ routes/push-router.js
@@ -1,6 +1,21 @@
 const fetch = require('node-fetch')
-
+const p = require('node-prowl')
 const apikey = process.env.PROWL_KEY
-const url = 'https://api.prowlapp.com/publicapi/add'
 
-module.exports = {}
+const prowl = new p(apikey);
+//const url = 'https://api.prowlapp.com/publicapi/add'
+
+const push = (msg) =>{
+
+prowl.push('YO, this is awesomez!', 'My app', {
+	providerkey: 'my-provider-key',
+	priority: 2,
+	url: 'http://revrise.com'
+}, function( err, remaining ){
+	if( err ) throw err;
+	console.log( 'I have ' + remaining + ' calls to the api during current hour. BOOM!' );
+});
+
+}
+
+module.exports = push
