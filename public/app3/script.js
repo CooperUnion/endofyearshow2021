5f0e9b72a574fc0a59c51e26403b68efd5bbf8be
Glitch (eoys-uploader-2021) <none>
2021-05-02 23:48:44 +0000

    🍗🎭 Checkpoint ./public/app3/script.js:226065/334 ./views/app3.html:226065/159

--- public/app3/script.js
+++ public/app3/script.js
@@ -1,9 +1,25 @@
 import * as Vue from './js/vue@3.0.11/dist/vue.esm-browser.min.js'
 import VueRouter from './js/vue-router@3.5.1/dist/vue-router.esm.browser.min.js'
-import * as VueLoader from './js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js'
+import { loadModule }from './js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js'
 import Vuex from './js/vuex@3.6.2/dist/vuex.esm.browser.min.js'
-
-const { loadModule } = VueLoader
 console.log("loaded fine")
 
-Vue.createApp()
+
+
+
+
+const Counter = {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.counter++
+      console.log(this.counter)
+    }, 1000)
+  }
+}
+
+Vue.createApp(Counter).mount('#counter')
