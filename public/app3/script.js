153926cc886238e0e8b4db10ec44e4bd7b204149
Glitch (eoys-uploader-2021) <none>
2021-05-02 23:25:43 +0000

    🏬🛋 Checkpoint ./public/app3/script.js:226065/116 ./public/app3/js/vue@3.0.11/dist/vue.esm-browser.min.js:226065/559308

--- public/app3/script.js
+++ public/app3/script.js
@@ -1,5 +1,5 @@
-import * as Vue from './js/vue@3.0.11/dist/vue.global.js'
+import * as Vue from './js/vue@3.0.11/dist/vue.esm-browser.min.js'
+import * as VueRouter from './js/vue-router@3.5.1/dist/vue-router.min.js'
 
 console.log("loaded fine")
 
-new Vue()
