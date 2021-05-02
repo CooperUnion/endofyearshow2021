04854cd9b6f0ba00903a8d6c671893eff19a7cd9
Glitch (eoys-uploader-2021) <none>
2021-05-02 23:37:19 +0000

    🚕🚏 Checkpoint ./public/app3/js/vue-router@3.5.1/dist/vue-router.esm.browser.js:226065/26954 ./public/app3/script.js:226065/708 ./public/app3/js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js:226065/1399234 ./public/app3/js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js.map:226065/4574207 ./public/app3/js/vuex@3.6.2/dist/vuex.esm.browser.min.js:226065/10592

--- public/app3/script.js
+++ public/app3/script.js
@@ -1,5 +1,9 @@
 import * as Vue from './js/vue@3.0.11/dist/vue.esm-browser.min.js'
-import * as VueRouter from './js/vue-router@3.5.1/dist/vue-router.min.js'
+import VueRouter from './js/vue-router@3.5.1/dist/vue-router.esm.browser.min.js'
+import * as VueLoader from './js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js'
+import Vuex from './js/vuex@3.6.2/dist/vuex.esm.browser.min.js'
 
+const { loadModule } = VueLoader
 console.log("loaded fine")
 
+Vue.createApp()
