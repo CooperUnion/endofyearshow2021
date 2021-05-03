501b99904cb05431f995edbe5e18795688401864
Glitch (eoys-uploader-2021) <none>
2021-05-03 03:15:09 +0000

    💨🔦 Checkpoint ./public/app3/script.js:226065/147 ./public/app3/js/vue-router@4.0.6/dist/vue-router.esm-browser.min.js:226065/42849

--- public/app3/script.js
+++ public/app3/script.js
@@ -1,5 +1,5 @@
 import * as Vue from './js/vue@3.0.11/dist/vue.esm-browser.min.js'
-import VueRouter from './js/vue-router@3.5.1/dist/vue-router.esm.browser.min.js'
+import { createRouter } from './js/vue-router@4.0.6/dist/vue-router.esm-browser.min.js'
 import { loadModule } from './js/vue3-sfc-loader@0.7.3/dist/vue3-sfc-loader.esm.js'
 import Vuex from './js/vuex@3.6.2/dist/vuex.esm.browser.min.js'
 import options from './js/loadModuleOptions.js'
