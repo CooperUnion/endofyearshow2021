7d6db9fe46b8f42b0a35ddc0319a41f15a25fba0
Glitch (eoys-uploader-2021) <none>
2021-05-04 00:51:02 +0000

    🚅🏢 Checkpoint ./routes/api-router.js:226065/461 ./lib/projects.js:226065/30

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Special = require('../lib/projects')
+const Special = require('../lib/projects');
+const special = new Special();
+
+const multer = require('multer');
+const memoryStorage = multer.memoryStorage()
+const upload = multer({ storage: memoryStorage })
 
 
 const {
@@ -25,7 +30,12 @@ router.get('/posts/:id', apiLogger, async (req, res, next)=>{
 })
 
 router.get('/projects', apiLogger, async (req, res, next)=>{
-  
+  return res.json(special.projects)
+})
+
+router.post('/projects', apiLogger, upload.none(), async (req, res, next)=>{
+  special.projects = req.body.projectName
+  return res.json(req.body.projectName)
 })
 
 module.exports = router
