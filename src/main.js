42c3393b70fa69e67f1647f37fd42da135cabf0e
Glitch (eoys-uploader-2021) <none>
2021-05-07 00:25:01 +0000

    📈🏚 Checkpoint ./src/assets/css/tacit-css-1.5.5.min.css:20381007/5625 ./src/main.js:20381007/132

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './public/css/tacit-css-1.5.5.min.css';
+// import 'https://cdn.jsdelivr.net/gh/yegor256/tacit@gh-pages/tacit-css-1.5.5.min.css';
 
 createApp(App).use(store).use(router).mount('#app')
