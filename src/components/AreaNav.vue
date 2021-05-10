47e9e29dd2a121792184b9cb125a201802221e2b
Glitch (endofyearshow2021) <none>
2021-05-10 19:35:06 +0000

    🎬👔 Checkpoint ./src/components/AreaNav.vue:20381007/628

--- src/components/AreaNav.vue
+++ src/components/AreaNav.vue
@@ -1,6 +1,6 @@
 <template>
   <ul id="areanav" class="nav-list">
-    <li v-for="item in items" :key="item">
+    <li class="nav-item" v-for="item in items" :key="item">
       <area-tag :data-tagname="item.name.toLowerCase().replace(/\s+/g, '-')" />
       <router-link :to="item.url">{{item.name}}</router-link>
       <output>##</output>
@@ -24,21 +24,21 @@
 
 <style scoped>
   
-  .nav-list li {
+  .nav-list .nav-item {
     display: flex;
     margin-bottom: 1.5em;
     font-size: 16px;
     line-height: 1.5;
  }
 
-  .nav-list li a {
+  .nav-list .nav-item a {
     color: #000;
     font-weight: 700;
     display: inline-block;
   }
 
 
-  .nav-list li output {
+  .nav-list .nav-item output {
     color: #999;
     margin-left: 0.25em;
     font-size: 16px;
@@ -46,6 +46,20 @@
     font-style: normal;
   }
 
+
+  .nav-list .nav-item output {
+    color: #999;
+    margin-left: 0.25em;
+    font-size: 16px;
+    line-height: 1.5;
+    font-style: normal;
+  }
+
+  .nav-list .nav-item .tag {
+    height: 24px;
+    width: 24px;
+    margin-right: 12px;
+  }
 
 /*   .nav-list li:before {
   	content: "";
