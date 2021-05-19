a8f217831cf251e474293c3f2fc366bb4bc39055
Glitch (endofyearshow2021) <none>
2021-05-19 17:24:35 +0000

    📈🛢 Checkpoint ./src/components/Posts.vue:20381007/22 ./src/components/PostScrim.vue:20381007/745

--- src/components/PostScrim.vue
+++ src/components/PostScrim.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="post-scrim">
-    {{media}}
-    SCRIM
-    <img />
+    <div class="scrim-contents">
+      <img :src="media.source_url" />
+    </div>
   </div>
 </template>
 
@@ -16,4 +16,27 @@
 </script>
 
 <style scoped>
+  .post-scrim {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: #000;
+    color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  
+  .post-scrim .scrim-contents {
+    margin: 48px 120px;
+  }
+
+  .post-scrim .scrim-contents img {
+    max-width: 100%;
+    height: auto;
+  }
+
+  
 </style>
