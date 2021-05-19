dffb0fbf4f2eac55cd6271b865f98d2f0324a9a2
Glitch (endofyearshow2021) <none>
2021-05-19 17:00:40 +0000

    😜🗻 Checkpoint ./src/components/PostScrim.vue:20381007/4463

---
+++ src/components/PostScrim.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :href="media.source_url">
+    <img v-if="!media.sizes.medium_large" :src="media.source_url" />
+    <img v-if="media.sizes.medium_large" :src="media.sizes.medium_large.source_url" />
+  </a>
+
+</template>
+
+<script>
+  export default {
+    name: 'PostScrim',
+  }
+</script>
+
+<style scoped>
+</style>
