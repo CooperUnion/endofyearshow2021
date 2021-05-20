273c4bf1be35e61a6ddce6fea7c09e5053f7e645
Glitch (endofyearshow2021) <none>
2021-05-20 14:36:51 +0000

    🍃🌘 Checkpoint ./src/components/Projects.vue:20381007/2402

---
+++ src/components/Projects.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="projectBox">
+    <div class="item" v-for="item in items" v-bind:key="item.id">
+      <!-- block item markup -->
+        <div class="post" v-if="item.id">
+          <post-media @click.prevent="loadScrim(item.id)" 
+            :media="item.assets.preview" />
+          <post-info
+            :tags="item.taxonomy.tags"
+            :title="item.title"
+            :author="item.author"
+            :post="item.id" />
+          <post-scrim v-if="displayScrim(item.id)"
+            :tags="item.taxonomy.tags"
+            :title="item.title"
+            :author="item.author"
+            :assets="item.assets"
+            :meta="item.meta"
+            :post="item.id"
+            :type="item.type"/>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { computed, ref, watch } from 'vue'
+  import { useStore } from 'vuex'  
+    
+  import PostInfo from '@/components/PostInfo.vue'
+  import PostMedia from '@/components/PostMedia.vue'
+  import PostScrim from '@/components/PostScrim.vue'
+  
+  export default {
+    name: 'Projects',
+    props: {
+      items: Array
+    },
+    components: {
+      PostInfo,
+      PostMedia,
+      PostScrim
+    },
+    setup(props){
+      const store = useStore()
+      
+      const loadScrim = (id) => {
+        store.commit('setActiveScrimId', id)
+      }
+      
+      const hideScrim = () => {
+        store.commit('resetActiveScrimId')
+      }
+      
+      const displayScrim = (id) => {
+        return id === store.state.activeScrimId
+      }
+      
+      return {loadScrim, displayScrim, hideScrim}
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
