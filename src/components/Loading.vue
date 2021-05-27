eda8ed46477a9853f2cf45a5d33bd656bca193e4
Glitch (remixed-endofyearshow2021) <none>
2021-05-27 19:11:47 +0000

    changed loading

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -34,7 +34,7 @@
 <style scoped>
   
   .loading {
-    background-image: url(<%= BASE_URL %>/img/loading.gif);
+    background-image: url(<%= BASE_URL %>/loading.gif);
     background-size: contain;
     width: 100px;
     height: 100px;
