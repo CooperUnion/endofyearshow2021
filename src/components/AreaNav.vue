12418ad75ab737d331797fc0b6a5be544f618246
Glitch (endofyearshow2021) <none>
2021-05-27 22:18:43 +0000

    👀🚅 Checkpoint ./src/components/AreaNav.vue:20381007/99

--- src/components/AreaNav.vue
+++ src/components/AreaNav.vue
@@ -209,11 +209,11 @@
  
     .nav-list {
       max-height: calc(100% - 96px);
-      transition: transform 0.333s cubic-bezier(0.34, 1.56, 0.64, 1);
+      transition: transform 0.3s cubic-bezier(0.68, -0.55, 0.265, 1.55)
     }
 
     #areanav:not(.showNav) .nav-list {
-      transform: translateX(-50vw);
+      transform: translateX(10vw);
     }
 
 
