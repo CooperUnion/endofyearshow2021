c0808d3a473a1608ffda03700e7b29ffeb815116
Glitch (remixed-endofyearshow2021) <none>
2021-05-31 04:42:20 +0000

    ✋💾 Checkpoint ./src/views/SocketDebug.vue:226065/420:6630263/87

--- src/views/SocketDebug.vue
+++ src/views/SocketDebug.vue
@@ -19,12 +19,14 @@
 
   import { ref, computed } from 'vue'
   import { useStore } from 'vuex'  
+  import $socket from '@/store/socket.js'
   
   export default {
   name: 'SocketDebug',
   components: {
   },
   setup(){
+
       const store = useStore()
       // const message = ref(store.state.socket.message)
       const message = computed(() => store.state.socket.message)
@@ -35,11 +37,22 @@
       
       const send = ()=>{
         // store.commit('...', 'test message from vue')
-        store.dispatch('socket_sendMessage', 'data from vue client')
-        console.log("send hit")
+        // store.dispatch('socket_sendMessage', 'data from vue client')
+        // console.log("send hit")
+        
+        $socket.client.emit('emit_method', val);
+//isn't socket accessible from this.$socket.client.emit without importing it?
       }      
       
     return {message, dump, send}
+  },
+  sockets: {
+    connect() {
+      console.log('socket connected')
+    },
+    customEmit(val) {
+      console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+    }
   }
 }
 </script>
