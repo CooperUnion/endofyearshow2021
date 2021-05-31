8f2d2f2f511507df9e05c1e9c6c35264e58f20e8
Glitch (remixed-endofyearshow2021) <none>
2021-05-31 01:45:05 +0000

    👱🎎 Checkpoint ./src/components/Cursors-attempt-rightvue.vue:6630263/19621 ./src/views/Cursors.vue:6630263/177

--- src/views/Cursors.vue
+++ src/views/Cursors.vue
@@ -6,6 +6,7 @@
 
  // import Cursors from '@/components/Cursorsonhiddenoverflow.vue'
  import Cursors from '@/components/Cursors-fullscreen.vue'
+   import Cursors from '@/components/Cursors-fullscreen.vue'
 
     export default {
   components: {
