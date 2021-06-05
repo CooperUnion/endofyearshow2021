51cf75c0e838718fd67a3748fa9177244f685072
Glitch (remixed-endofyearshow2021) <none>
2021-06-05 18:50:34 +0000

    🌸💪 Checkpoint ./src/components/Cursors-attempt-rightvue.vue:6630263/436

--- src/components/Cursors-attempt-rightvue.vue
+++ src/components/Cursors-attempt-rightvue.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="cursorscontainer" @mousemove="updatePlayerPos({event.clientX, event.clientY})">
+    <div id="cursorscontainer" @mousemove="mousemove($event)">
         <h1>Current message: {{message}}</h1>
   <ul>
     <li @click="update(Math.random()*1000)">Send a random number</li>
@@ -287,6 +287,10 @@
 //   },
 
   methods: {
+            mouseMove(event) {
+            console.log(event.clientX, event.clientY);
+              store.dispatch('client_playerCursorMove', `data from cursor movement, ${event}`)
+        },
     
     pingServer() {
       // Send the "pingServer" event to the server.
