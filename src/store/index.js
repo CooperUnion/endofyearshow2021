c9f9a53a08cf01959b38739153eff2e1ef1d51df
Glitch (remixed-endofyearshow2021) <none>
2021-06-06 23:04:52 +0000

    😬🐆 Checkpoint ./src/store/index.js:6630263/943

--- src/store/index.js
+++ src/store/index.js
@@ -22,6 +22,11 @@ const socket = {
     }
   },
   mutations: {
+    SOCKET_INIT_START(state, data){
+      console.log("socket init start", data)
+      const justUsers = data.friends.filter((item)=>{ return typeof item === 'object' })
+      justUsers.forEach(user => state.cursors.cursors[user.id] = user)
+    },
     SOCKET_USER_MESSAGE(state, message) {
       state.message = {message, origin: 'socket'}
       console.log("socketusermessagemutationMUTATION", message)
@@ -144,125 +149,129 @@ const socket = {
       
     },
     init({ dispatch, commit }, data){ //fires when first chosen, but not on new pageloads? how can we make it first on new pageloads?
-      console.log("init", data)
-   async function run(data, that, dispatch){
       
-  function promptPromise(message, message2) {
-  let dialog  = document.getElementById('dialog');
-  let input   = document.getElementById("textinput");
-  let okButton = dialog.querySelector('button.ok');
-  let XButton = dialog.querySelector('button.close-dialog');
-  const skipButton = dialog.querySelector('button.cancel')
-  let radio = ""
+      console.log("otherusermove", data)
+      commit('SOCKET_INIT_START', data)
+      
+//       console.log("init", data)
+//    async function run(data, that, dispatch){
+      
+//   function promptPromise(message, message2) {
+//   let dialog  = document.getElementById('dialog');
+//   let input   = document.getElementById("textinput");
+//   let okButton = dialog.querySelector('button.ok');
+//   let XButton = dialog.querySelector('button.close-dialog');
+//   const skipButton = dialog.querySelector('button.cancel')
+//   let radio = ""
   
-  dialog.querySelector('.message').innerHTML = String(message);
-  dialog.querySelector('.message2').innerHTML = String(message2)
-  dialog.className = '';
+//   dialog.querySelector('.message').innerHTML = String(message);
+//   dialog.querySelector('.message2').innerHTML = String(message2)
+//   dialog.className = '';
 
-  return new Promise(function(resolve, reject) {
-    dialog.onclick = function(e){
-      if (e.target.tagName !== 'BUTTON') { return; }
-      dialog.onclick = null;
-      dialog.className = 'hidden';
-      if (e.target === okButton) {
-let radios = document.getElementsByName('roleRadio');
-for (let i = 0, length = radios.length; i < length; i++) {
-  if (radios[i].checked) {
-   radio = radios[i].value
-   console.log(radio)
-    resolve({input: input.value, radio: radio});
-    break;
-  }
-}
+//   return new Promise(function(resolve, reject) {
+//     dialog.onclick = function(e){
+//       if (e.target.tagName !== 'BUTTON') { return; }
+//       dialog.onclick = null;
+//       dialog.className = 'hidden';
+//       if (e.target === okButton) {
+// let radios = document.getElementsByName('roleRadio');
+// for (let i = 0, length = radios.length; i < length; i++) {
+//   if (radios[i].checked) {
+//    radio = radios[i].value
+//    console.log(radio)
+//     resolve({input: input.value, radio: radio});
+//     break;
+//   }
+// }
 
-      } else if (e.target === XButton){
-      } else if (e.target === skipButton){
-        resolve({input: "", radio: "skipped"});
-      }
-    };
-  });
-}
+//       } else if (e.target === XButton){
+//       } else if (e.target === skipButton){
+//         resolve({input: "", radio: "skipped"});
+//       }
+//     };
+//   });
+// }
     
 
-  let button = document.getElementById('action');
-  let output = document.getElementById('prompt');
-  let rolefield = document.getElementById('role')
+//   let button = document.getElementById('action');
+//   let output = document.getElementById('prompt');
+//   let rolefield = document.getElementById('role')
   
-  if (window.sessionStorage.getItem('EOYS2021Name') && window.sessionStorage.getItem('EOYS2021Name') != ""){
-    let dialog  = document.getElementById('dialog');
-    if (dialog){
-    dialog.className = 'hidden'
-    }
-    console.log("session storage SUCCESS")
-    // Save data to sessionStorage
+//   if (window.sessionStorage.getItem('EOYS2021Name') && window.sessionStorage.getItem('EOYS2021Name') != ""){
+//     let dialog  = document.getElementById('dialog');
+//     if (dialog){
+//     dialog.className = 'hidden'
+//     }
+//     console.log("session storage SUCCESS")
+//     // Save data to sessionStorage
 
     
-          const response = {name: window.sessionStorage.getItem('EOYS2021Name'), role: window.sessionStorage.getItem('EOYS2021Role')}
-      // that.$socket.client.emit('nameChosen', {response: response, player: data.player})
-          window.sessionStorage.setItem('EOYS2021TempId', data.player)
-          dispatch('nameChosen',{response: response, player: data.player});
-      // console.log("IMPORTANT: emit nameChosen HERE", {response: response, player: data.player})
+//           const response = {name: window.sessionStorage.getItem('EOYS2021Name'), role: window.sessionStorage.getItem('EOYS2021Role')}
+//       // that.$socket.client.emit('nameChosen', {response: response, player: data.player})
+//           window.sessionStorage.setItem('EOYS2021TempId', data.player)
+//           dispatch('nameChosen',{response: response, player: data.player});
+//       // console.log("IMPORTANT: emit nameChosen HERE", {response: response, player: data.player})
     
-                      that.Meeting1 = new Meeting()  
-                      console.log(data)
-                      console.log("data")
-                      // data.friends.forEach(friend1 => console.log(friend1));
-                      data.friends.forEach(friend1 => that.Meeting1.createFriend(friend1.id, data.player, that.Meeting1, friend1.name, friend1.role, friend1));
-                      self.player = new Player(data.player);
+//                       that.Meeting1 = new Meeting()  
+//                       console.log(data)
+//                       console.log("data")
+//                       // data.friends.forEach(friend1 => console.log(friend1));
+//                       data.friends.forEach(friend1 => that.Meeting1.createFriend(friend1.id, data.player, that.Meeting1, friend1.name, friend1.role, friend1));
+//                       self.player = new Player(data.player);
 
-                      window.onmousemove = (e) => {
-                          // console.log(document.querySelector("body"))
-                          const x = e.clientX
-                          const y = e.pageY
-                          const location = self.player.update(x,y,dispatch, data.player, response.name, response.role) 
-                      };    
-      output.innerHTML = '' + response.name;
-      rolefield.innerHTML = "" + response.role;
+//                       window.onmousemove = (e) => {
+//                           // console.log(document.querySelector("body"))
+//                           const x = e.clientX
+//                           const y = e.pageY
+//                           const location = self.player.update(x,y,dispatch, data.player, response.name, response.role) 
+//                       };    
+//       output.innerHTML = '' + response.name;
+//       rolefield.innerHTML = "" + response.role;
 
-// Get saved data from sessionStorage
-// let data = sessionStorage.getItem('key');
+// // Get saved data from sessionStorage
+// // let data = sessionStorage.getItem('key');
     
-  }else{
-    var dialog  = document.getElementById('dialog');
-    dialog.classList.remove("hidden")
-    console.log("session storage FAILED")
-    promptPromise('Welcome to the Cooper Union School of Art End of Year Show 2021!', 'Would you like your cursor to be visible while you move <br> through the galleries?').then(function(name) {
-      output.innerHTML = '' + name.input;
-      rolefield.innerHTML = "" + name.radio;
-      console.log("response completed!")
-      // console.log(that.$socket.client)
-      const response = {name: name.input, role: name.radio}
-      window.sessionStorage.setItem('EOYS2021Name', name.input)
-      window.sessionStorage.setItem('EOYS2021Role', name.radio)
-      // that.$socket.client.emit('nameChosen', {response: response, player: data.player})
-      // console.log("IMPORTANT: emit nameChosen HERE", {response: response, player: data.player})
-      dispatch('nameChosen',{response: response, player: data.player});
-                      that.Meeting1 = new Meeting()  
-                      console.log(data)
-      console.log("data")
-                      // data.friends.forEach(friend1 => console.log(friend1));
-                      data.friends.forEach(friend1 => that.Meeting1.createFriend(friend1.id, data.player, that.Meeting1, friend1.name, friend1.role, friend1));
-                      self.player = new Player(data.player);
+//   }else{
+//     var dialog  = document.getElementById('dialog');
+//     dialog.classList.remove("hidden")
+//     console.log("session storage FAILED")
+//     promptPromise('Welcome to the Cooper Union School of Art End of Year Show 2021!', 'Would you like your cursor to be visible while you move <br> through the galleries?').then(function(name) {
+//       output.innerHTML = '' + name.input;
+//       rolefield.innerHTML = "" + name.radio;
+//       console.log("response completed!")
+//       // console.log(that.$socket.client)
+//       const response = {name: name.input, role: name.radio}
+//       window.sessionStorage.setItem('EOYS2021Name', name.input)
+//       window.sessionStorage.setItem('EOYS2021Role', name.radio)
+//       // that.$socket.client.emit('nameChosen', {response: response, player: data.player})
+//       // console.log("IMPORTANT: emit nameChosen HERE", {response: response, player: data.player})
+//       dispatch('nameChosen',{response: response, player: data.player});
+//                       that.Meeting1 = new Meeting()  
+//                       console.log(data)
+//       console.log("data")
+//                       // data.friends.forEach(friend1 => console.log(friend1));
+//                       data.friends.forEach(friend1 => that.Meeting1.createFriend(friend1.id, data.player, that.Meeting1, friend1.name, friend1.role, friend1));
+//                       self.player = new Player(data.player);
 
-                      window.onmousemove = (e) => {
-                          // console.log(document.querySelector("body"))
-                          const x = e.clientX
-                          const y = e.pageY
-                          const location = self.player.update(x,y,dispatch, data.player, name.input, name.radio) 
-                      };    
-      window.sessionStorage.setItem('EOYS2021TempId', data.player)
+//                       window.onmousemove = (e) => {
+//                           // console.log(document.querySelector("body"))
+//                           const x = e.clientX
+//                           const y = e.pageY
+//                           const location = self.player.update(x,y,dispatch, data.player, name.input, name.radio) 
+//                       };    
+//       window.sessionStorage.setItem('EOYS2021TempId', data.player)
 
-    }).catch(function() {
-      // output.innerHTML = '¯\\_(ツ)_/¯';
-      console.log("ERROR?")
-    }); 
-     
-     
-     
-  }
-   }
-      let that = this;
-      run(data, that, dispatch);
+//     }).catch(function() {
+//       // output.innerHTML = '¯\\_(ツ)_/¯';
+//       console.log("ERROR?")
+//     }); 
+//      
+//      
+//      
+  // }
+  //  }
+  //     let that = this;
+  //     run(data, that, dispatch);
       
     },
     socketMove({ dispatch, commit }, data){
