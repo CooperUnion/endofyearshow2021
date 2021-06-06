7f5ea32eb317e97b22c0340f8c52f56bd28fc2a1
Glitch (remixed-endofyearshow2021) <none>
2021-06-06 20:35:17 +0000

    😬👯 Checkpoint ./src/router/index.js:6630263/310 ./src/views/MasonryPosts.vue:6630263/36 ./src/views/DebugCursorDisplay.vue:6630263/1498 ./src/components/CursorDisplay.vue:6630263/1794

---
+++ src/components/CursorDisplay.vue
@@ -0,0 +1,38 @@
+<template>
+<h1>
+  CursorDisplay is HERE
+  </h1>
+</template>
+
+<script>
+  import { ref, onBeforeMount } from "vue";
+  
+
+  export default {
+    name: 'Loading',
+    props: {
+      timeout: Number
+    },
+    setup(props){
+      const loading = ref(true)
+
+      async function autoTimeout(){
+        
+        setTimeout(()=>{
+          loading.value = false
+        }, props.timeout * 1000)
+        
+      }
+      onBeforeMount(autoTimeout)
+
+      return {loading}
+    }
+  }
+  
+</script>
+
+<style scoped>
+
+  
+</style>
+
---
+++ src/views/DebugCursorDisplay.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>
+    DebugCursorDisplay
+  </h1>
+  <cursor-display/>
+</template>
+<script>
+
+  import { ref, computed, inject, getCurrentInstance } from 'vue'
+  import { useStore } from 'vuex'  
+  import CursorDisplay from '@/components/CursorDisplay.vue'
+  
+  export default {
+    name: 'DebugCursorDisplay',
+    components:{
+      CursorDisplay
+    },
+    props:{
+      test: String
+    }
+  }
+</script>
