ccdc1eac910fef04648e15bcd5b25fd5be70a359
Erin Sparling <erin@everyplace.net>
2021-06-07 00:51:08 -0400

    New build from v0.7.0

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09b95628a1e29f1ce6532536c5f958ee.js"
+  "/precache-manifest.1f5b353c3626e57ff51c08d9ae559e9a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "endofyearshow2021"});
