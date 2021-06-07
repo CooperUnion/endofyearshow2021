da72d72a1b0a96257ef9be36b6083b85f6df5dda
Glitch (remixed-endofyearshow2021) <none>
2021-06-07 03:07:48 +0000

    🏮🌓 Checkpoint ./src/views/DebugCursorDisplay.vue:226065/1836

--- src/views/DebugCursorDisplay.vue
+++ src/views/DebugCursorDisplay.vue
@@ -1,22 +1,11 @@
 <template>
-  <h1>
-    DebugCursorDisplay
-  </h1>
-  <b @click="dump()">dump vuex</b>
 
   <cursor-display :player="player" />
-
-  <div>
-    
-  Current live data: {{player}} <br />
-
-  Data from vuex: {{storeRetreivedData}}
-      </div>
 
 </template>
 <script>
 
-  import { ref, computed, inject, getCurrentInstance } from 'vue'
+  import { ref, onBeforeMount } from 'vue'
   import { useStore } from 'vuex'  
   import CursorDisplay from '@/components/CursorDisplay.vue'
   
@@ -27,28 +16,23 @@
     },
     props:{},
     setup(props){
-
+      
       const player = ref({})
       player.value.name = "ricky"
       player.value.type = "friend-cu"
       player.value.id = 10
       const store = useStore()
 
-      window.onmousemove = (e) => {
-          const x = e.clientX
-          const y = e.pageY
-          player.value.position = {x,y}
-          store.dispatch('move', player.value)
+       window.onmousemove = (e) => {
+        const x = e.clientX
+        const y = e.pageY
+        player.value.position = {x,y}
+        store.dispatch('move', player.value)
       }
 
       const storeRetreivedData = ref(store.state.socket)
 
-
-      const dump = ()=>{
-        console.log(store.state.socket)
-      }
-
-      return { player, dump }
+      return { player }
     }
   }
 </script>
