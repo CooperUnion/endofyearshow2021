87e48457cfb3c86be14f509088b8bfda740c0c26
Erin Sparling <erin@everyplace.net>
2021-06-06 21:19:36 -0400

    Added an s, contextually

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -37,7 +37,7 @@
       const activeGlobal = store.state.application.activeGlobal
       
       const isActiveRoute = (current) => {
-        if(current === route.name) {
+        if(current === route.name || current === route.name + 's') {
           store.commit('setGlobalNav', current)
           return 'active'
         }
